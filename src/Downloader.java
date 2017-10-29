386e6f38f29d3b6147eebe9188661661eb382acb
Tad <tad@spotco.us>
2017-10-29 13:23:45 -0400

    Actually download the files

--- src/Downloader.java
+++ src/Downloader.java
@@ -1,11 +1,17 @@
+import javax.swing.*;
 import java.io.File;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Downloader {
 
     private static String cveJson = "/home/***REMOVED***/Downloads/cves";
-    private static String output = "/mnt/Drive-1/Development/Other/Android_ROMs/Patches/Linux_CVEs-New";
+    private static String output = "/mnt/Drive-1/Development/Other/Android_ROMs/Patches/Linux_CVEs-New/";
     private static ArrayList<CVE> cves = new ArrayList<CVE>();
 
     public static void main(String[] args) {
@@ -56,13 +62,24 @@ public class Downloader {
             e.printStackTrace();
         }
 
+        System.out.println("Downloading patches...");
+        int c = 0;
         for(CVE cve : cves) {
             //Only run if we have patches available
             if(cve.getLinks().size() > 0) {
                 //Iterate over all links and download if needed
+                int linkC = 0;
                 for (Link link : cve.getLinks()) {
-
+                    File outDir = new File(output + cve.getId());
+                    outDir.mkdirs();
+                    downloadFile(getPatchURL(link), new File(outDir.getAbsolutePath() + "/" + linkC + ".patch"), true);
+                    System.out.println("\tDownloaded " + link.getURL());
+                    linkC++;
+                    c++;
                 }
+            }
+            if(c == 10) {
+                break;
             }
         }
     }
@@ -73,11 +90,33 @@ public class Downloader {
         } else if(link.getURL().contains("git.kernel.org") || link.getURL().contains("source.codeaurora.org")) {
             return link.getURL().replaceAll("commit", "patch");
         } else if(link.getURL().contains("android.googlesource.com")) {
-            return "";
+            return link.getURL() + "?format=TEXT"; //BASE64 ENCODED
         } else if(link.getURL().contains("review.lineageos.org")) {
-            return "";
+            String id = link.getURL().split("/")[3];
+            //TODO: Dynamically get revision
+            return "https://review.lineageos.org/changes/" + id + "/revisions/1/patch?download"; //BASE64 ENCODED
         }
         return "";
+    }
+
+    public static void downloadFile(String url, File out, boolean useCache) {
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setConnectTimeout(45000);
+            connection.setReadTimeout(45000);
+            connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; rv:6.0) Gecko/20100101 Firefox/19.0");
+            if (useCache && out.exists()) {
+                connection.setIfModifiedSince(out.lastModified());
+            }
+            connection.connect();
+            int res = connection.getResponseCode();
+            if (res != 304 && (res == 200 || res == 301 || res == 302)) {
+                Files.copy(connection.getInputStream(), out.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            }
+            connection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static class CVE {
