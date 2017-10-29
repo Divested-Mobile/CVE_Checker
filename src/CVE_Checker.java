0b6ac885f29e6ef5a564c85567ed147cbd83a5f3
Tad <tad@spotco.us>
2017-10-29 16:18:41 -0400

    Downloader fixes and checker version support

--- src/CVE_Checker.java
+++ src/CVE_Checker.java
@@ -1,4 +1,7 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -6,18 +9,18 @@ import java.util.Scanner;
 public class CVE_Checker {
 
     private static String prefix = "android_kernel_";
-    private static String patches = "/mnt/Drive-1/Development/Other/Android_ROMs/Patches/Linux_CVEs/";
+    private static String patches = "/mnt/Drive-1/Development/Other/Android_ROMs/Patches/Linux_CVEs-New/";
     private static String patchesScript = "\\$cvePatches/";
     private static String base = "/mnt/Drive-1/Development/Other/Android_ROMs/Build/LineageOS-14.1/";
     private static String outputBase = "/mnt/Drive-1/Development/Other/Android_ROMs/Scripts/LineageOS-14.1/CVE_Patchers/";
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        while(s.hasNextLine()) {
+        while (s.hasNextLine()) {
             String kernelName = prefix + s.nextLine();
             String kernelPath = kernelName.replaceAll("android_", "").replaceAll("_", "/");
             String kernel = base + kernelPath;
-            if(new File(kernel).exists()) {
+            if (new File(kernel).exists()) {
                 genScript(kernelName, kernelPath, kernel);
             } else {
                 System.out.println("Kernel does not exist");
@@ -28,57 +31,80 @@ public class CVE_Checker {
     public static void genScript(String kernelName, String kernelPath, String kernel) {
         String output = outputBase + kernelName + ".sh";
         ArrayList<String> scriptCommands = new ArrayList<String>();
+        String kernelVersion = "ERROR";
 
+        try {
+            Scanner kernelMakefile = new Scanner(new File(base + kernelPath + "/Makefile"));
+            while(kernelMakefile.hasNextLine()) {
+                String line = kernelMakefile.nextLine();
+                if(line.startsWith("VERSION = ")) {
+                    kernelVersion = line.split("= ")[1];
+                }
+                if(line.startsWith("PATCHLEVEL = ")) {
+                    kernelVersion += "." + line.split("= ")[1];
+                }
+                if(line.startsWith("NAME = ")) {
+                    break;
+                }
+            }
+            kernelMakefile.close();
+            System.out.println("DETECTED VERSION " + kernelVersion);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         File[] cves = new File(patches).listFiles(File::isDirectory);
-        if(cves != null && cves.length > 0) {
+        if (cves != null && cves.length > 0) {
             Arrays.sort(cves);
             for (File cve : cves) {
                 String cveReal = cve.toString().split("/")[8];
                 System.out.println("Checking " + cveReal);
-                File[] cveSubs = cve.listFiles(File::isFile);
-                if (cveSubs != null && cveSubs.length > 0) {
-                    Arrays.sort(cveSubs);
-                    Runtime rt = Runtime.getRuntime();
-                    int exitCounter = 0;
-                    ArrayList<String> commands = new ArrayList<String>();
-                    for (File cveSub : cveSubs) {
-                        try {
-                            String command = "git -C " + kernel + " apply --check " + cveSub.toString();
-                            commands.add(command.replaceAll(" --check", ""));
-                            System.out.println("\tTesting patch: " + command);
-                            Process git = rt.exec(command);
-                            while (git.isAlive()) {
-                                //Do nothing
-                            }
-                            exitCounter += git.exitValue();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    if (exitCounter == 0) {
-                        System.out.println("\tSuccessfully able to patch " + cveReal);
-                        for(String command : commands) {
+                String[] versions = new String[] {kernelVersion, "ANY"};
+                for (String version : versions) {
+                    File[] cveSubs = new File(cve.getAbsolutePath() + "/" + version + "/").listFiles(File::isFile);
+                    if (cveSubs != null && cveSubs.length > 0) {
+                        Arrays.sort(cveSubs);
+                        Runtime rt = Runtime.getRuntime();
+                        int exitCounter = 0;
+                        ArrayList<String> commands = new ArrayList<String>();
+                        for (File cveSub : cveSubs) {
                             try {
-                                System.out.println("\tApplying patch: " + command);
+                                String command = "git -C " + kernel + " apply --check " + cveSub.toString();
+                                commands.add(command.replaceAll(" --check", ""));
+                                System.out.println("\tTesting patch: " + command);
                                 Process git = rt.exec(command);
                                 while (git.isAlive()) {
                                     //Do nothing
                                 }
-                                if(git.exitValue() != 0) {
-                                    System.out.println("Failed: " + command);
-                                    System.exit(1);
-                                }
+                                exitCounter += git.exitValue();
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
-                            String commandScript = command.replaceAll(" -C " + kernel, "").replaceAll(patches, patchesScript);
-                            scriptCommands.add(commandScript);
+                        }
+                        if (exitCounter == 0) {
+                            System.out.println("\tSuccessfully able to patch " + cveReal);
+                            for (String command : commands) {
+                                try {
+                                    System.out.println("\tApplying patch: " + command);
+                                    Process git = rt.exec(command);
+                                    while (git.isAlive()) {
+                                        //Do nothing
+                                    }
+                                    if (git.exitValue() != 0) {
+                                        System.out.println("Failed: " + command);
+                                        System.exit(1);
+                                    }
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                                String commandScript = command.replaceAll(" -C " + kernel, "").replaceAll(patches, patchesScript);
+                                scriptCommands.add(commandScript);
+                            }
+                        } else {
+                            System.out.println("\tPatch(es) do(es) not apply cleanly");
                         }
                     } else {
-                        System.out.println("\tPatch(es) do(es) not apply cleanly");
+                        System.out.println("\tNo patches available");
                     }
-                } else {
-                    System.out.println("\tNo patches available");
                 }
             }
         } else {
@@ -91,7 +117,7 @@ public class CVE_Checker {
             PrintWriter out = new PrintWriter(output, "UTF-8");
             out.println("#!/bin/bash");
             out.println("cd $base\"" + kernelPath + "\"");
-            for(String cmd : scriptCommands) {
+            for (String cmd : scriptCommands) {
                 out.println(cmd);
             }
             out.println("cd $base");
--- src/Downloader.java
+++ src/Downloader.java
@@ -1,16 +1,14 @@
-import javax.swing.*;
 import java.io.File;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Downloader {
 
-    private static final String[] linuxVersions = new String[]{"3.0", "3.2", "3.4", "3.8", "3.10", "3.12", "3.16", "3.18", "4.4", "4.5", "4.8"};
+    private static final String[] linuxVersions = new String[] {"3.0", "3.2", "3.4", "3.8", "3.10", "3.12", "3.16", "3.18", "4.4", "4.5", "4.8"};
     private static String cveJson = "/home/***REMOVED***/Downloads/cves";
     private static String output = "/mnt/Drive-1/Development/Other/Android_ROMs/Patches/Linux_CVEs-New/";
     private static ArrayList<CVE> cves = new ArrayList<CVE>();
@@ -24,44 +22,44 @@ public class Downloader {
             String name = "";
             ArrayList<Link> links = new ArrayList<Link>();
             String curNotes = "";
-            while(cve.hasNextLine()) {
+            while (cve.hasNextLine()) {
                 String line = cve.nextLine();
                 String[] lineS = line.split("\"");
-                if(line.contains("cve_name") || !cve.hasNextLine()) {
-                    if(name.length() > 0) {
+                if (line.contains("cve_name") || !cve.hasNextLine()) {
+                    if (name.length() > 0) {
                         cves.add(new CVE(name, curNotes, links));
                         System.out.println("\t\tAdded " + links.size() + " links");
                         links = new ArrayList<Link>();
                         name = curNotes = "";
                     }
-                    if(cve.hasNextLine()) {
+                    if (cve.hasNextLine()) {
                         name = lineS[3];
                         System.out.println("\t" + name);
                     }
                 }
-                if(line.contains("\"cve_id\"")) {
+                if (line.contains("\"cve_id\"")) {
                     cve.nextLine();//oid
                     cve.nextLine();//}
                     line = cve.nextLine();
                     String desc = "";
                     String link = "";
-                    if(line.contains("\"desc\"")) {
+                    if (line.contains("\"desc\"")) {
                         desc = line.split("\"")[3];
                     }
-                    if(line.contains("\"link\"")) {
+                    if (line.contains("\"link\"")) {
                         link = line.split("\"")[3];
                     } else {
                         line = cve.nextLine();
-                        if(line.contains("\"link\"")) {
+                        if (line.contains("\"link\"")) {
                             link = line.split("\"")[3];
                         }
                     }
-                    if(link.length() > 0) {
+                    if (link.length() > 0) {
                         links.add(new Link(link, desc));
                         System.out.println("\t\tAdded a new link to " + link);
                     }
                 }
-                if(line.contains("\"notes\"")) {
+                if (line.contains("\"notes\"")) {
                     //curNotes = lineS[3];
                     //System.out.println("\t\tAdded a new note: " + curNotes);
                 }
@@ -73,22 +71,22 @@ public class Downloader {
 
         System.out.println("Downloading patches...");
         int c = 0;
-        for(CVE cve : cves) {
+        for (CVE cve : cves) {
             System.out.println("\t" + cve.getId());
             //Only run if we have patches available
-            if(cve.getLinks().size() > 0) {
+            if (cve.getLinks().size() > 0) {
                 //Iterate over all links and download if needed
                 int linkC = 0;
                 for (Link link : cve.getLinks()) {
                     String patch = getPatchURL(link);
-                    if(!patch.equals("NOT A PATCH")) {
+                    if (!patch.equals("NOT A PATCH")) {
                         File outDir = new File(output + cve.getId() + "/" + getPatchVersion(cve, link));
                         outDir.mkdirs();
                         String base64 = "";
-                        if(isBase64Encoded(link)) {
+                        if (isBase64Encoded(link)) {
                             base64 = ".base64";
                         }
-                        downloadFile(getPatchURL(link), new File(outDir.getAbsolutePath() + "/" + linkC + ".patch" + base64), true);
+                        downloadFile(patch, new File(outDir.getAbsolutePath() + "/" + linkC + ".patch" + base64), true);
                         System.out.println("\t\tDownloaded " + link.getURL());
                         linkC++;
                     }
@@ -102,13 +100,13 @@ public class Downloader {
     }
 
     private static String getPatchURL(Link link) {
-        if(link.getURL().contains("github.com")) {
+        if (link.getURL().contains("github.com")) {
             return link.getURL() + ".patch";
-        } else if(link.getURL().contains("git.kernel.org") || link.getURL().contains("source.codeaurora.org")) {
+        } else if (link.getURL().contains("git.kernel.org") || link.getURL().contains("source.codeaurora.org")) {
             return link.getURL().replaceAll("commit", "patch");
-        } else if(link.getURL().contains("android.googlesource.com")) {
+        } else if (link.getURL().contains("android.googlesource.com")) {
             return link.getURL() + "?format=TEXT"; //BASE64 ENCODED
-        } else if(link.getURL().contains("review.lineageos.org")) {
+        } else if (link.getURL().contains("review.lineageos.org") && !link.getURL().contains("topic")) {
             String id = link.getURL().split("/")[3];
             //TODO: Dynamically get revision
             return "https://review.lineageos.org/changes/" + id + "/revisions/1/patch?download"; //BASE64 ENCODED
@@ -117,7 +115,7 @@ public class Downloader {
     }
 
     private static boolean isBase64Encoded(Link link) {
-        if(link.getURL().contains("android.googlesource.com") || link.getURL().contains("review.lineageos.org")) {
+        if (link.getURL().contains("android.googlesource.com") || link.getURL().contains("review.lineageos.org")) {
             return true;
         }
         return false;
@@ -125,12 +123,12 @@ public class Downloader {
 
     private static String getPatchVersion(CVE cve, Link link) {
         String result = "";
-        for(String version : linuxVersions) {
-            if(link.getDesc().contains(version)) {
+        for (String version : linuxVersions) {
+            if (link.getDesc().contains(version)) {
                 result = version;
             }
         }
-        if(result.length() == 0) {
+        if (result.length() == 0) {
             result = "ANY";
         }
         return result;
@@ -179,6 +177,7 @@ public class Downloader {
             return links;
         }
     }
+
 
     public static class Link {
         private String url;
