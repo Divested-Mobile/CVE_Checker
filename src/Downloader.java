52958155038c5690d2a5fed920f2a4d6649d51d6
Tad <tad@spotco.us>
2019-05-14 18:30:21 -0400

    Support downloading patches from marc.info

--- src/Downloader.java
+++ src/Downloader.java
@@ -138,7 +138,9 @@ public class Downloader {
 
     private static String getPatchURL(Link link) {
         String url = link.getURL().replaceAll("http://", "https://");
-        if(url.contains("lkml.org/lkml/diff") || (url.contains("raw.githubusercontent") && url.endsWith(".patch"))) {
+        if(url.contains("lkml.org/lkml/diff")
+            || (url.contains("raw.githubusercontent") && url.endsWith(".patch"))
+            || (url.contains("marc.info") && url.endsWith("q=raw"))) {
             return url;
         } else if (url.contains("github.com")) {
             return url + ".patch";
