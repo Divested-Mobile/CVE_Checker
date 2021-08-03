536d071295e48c94d54b4b8e57d9f2f7b2de7b03
Tad <tad@spotco.us>
2021-08-03 13:54:18 -0400

    Work on supporting patches with copyright edits
    Signed-off-by: Tad <tad@spotco.us>

--- src/Patcher.java
+++ src/Patcher.java
@@ -269,6 +269,12 @@ public class Patcher {
     String command = "git -C " + repoPath + " apply --check " + patch;
     if (alternateRoot.length() > 0) {
       command += " --directory=\"" + alternateRoot + "\"";
+      //Patches often fail to apply merely because the copyright date is modified
+      //This following sed line removes the matching copyright blocks from a patch file
+      //sed '/^@@ -1.* +1.* @@/,/^@@ /{/^\(^@@ -1.* +1.* @@\|^  *\|^- \* Copyright.*\|^+ \* Copyright.*\)/d}'
+      //However it does not appear to work
+      //It should let CVE-2020-11115/qcacld-2.0/0002.patch apply to kernel/oneplus/msm8996 successfully, but it fails
+      //command = "bash -c \"sed '/^@@ -1.* +1.* @@/,/^@@ /{/^\\(^@@ -1.* +1.* @@\\|^  *\\|^- \\* Copyright.*\\|^+ \\* Copyright.*\\)/d}' " + patch + " | git -C " + repoPath + " apply --check --directory=\"\\" + alternateRoot + "\\\" -\"";
     }
     if (patch.contains("0001-LinuxIncrementals")) {
       command += " --exclude=Makefile";
